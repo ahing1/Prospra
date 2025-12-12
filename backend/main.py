ef81dd298404279f0189a6dba841d2b7212d6bd4
ahing1 <ahing1@stevens.edu>
2025-12-11 23:10:48 -0500

    final changes for deployment

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,9 @@ app.include_router(pro.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend dev server
+    allow_origins=[
+        "http://localhost:3000",
+        "http://18.188.39.185:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
